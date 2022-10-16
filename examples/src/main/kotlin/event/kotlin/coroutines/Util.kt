ca5e5872ebc5c6be755afea8baad72bafb8fbf94
adityasofat <>
2022-10-16 15:51:21 +0100

    WIP - Flow test

--- examples/src/main/kotlin/event/kotlin/coroutines/Util.kt
+++ examples/src/main/kotlin/event/kotlin/coroutines/Util.kt
@@ -3,4 +3,4 @@ package event.kotlin.coroutines
 fun threadName() = "[${Thread.currentThread().name}]"
 
 fun log(message : String) = println("${threadName()} $message")
-fun log(message : Int) = println("${threadName()} $message")
+fun log(message : Int) = log(message.toString())
